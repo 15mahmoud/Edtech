f22538c32a85ac3c8560ddbec4d9853c9c153745
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2024-12-08 19:39:37 +0200

    add new additon info screen

--- app/src/main/java/com/example/student_project/screen/login/Login.kt
+++ app/src/main/java/com/example/student_project/screen/login/Login.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -19,6 +20,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -27,10 +30,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -44,9 +50,14 @@ import com.example.student_project.ui.theme.borderButton
 import com.example.student_project.ui.theme.buttonColor
 import com.example.student_project.ui.theme.headLineColor
 import com.example.student_project.ui.theme.textFieldColor
+import kotlinx.coroutines.launch
 
 @Composable
 fun LoginScreen(navController: NavController) {
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+    val scope = rememberCoroutineScope()
     var emailState by remember {
         mutableStateOf("")
     }
@@ -98,8 +109,14 @@ fun LoginScreen(navController: NavController) {
                         emailError = it.isEmpty()
                     },
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp)
+                        .width(screenWidth * 90 / 100)
+                        .align(alignment = Alignment.CenterHorizontally)
+                        .shadow(
+                            elevation = 6.dp,
+                            shape = MaterialTheme.shapes.small,
+                            ambientColor = Color.Gray,
+                            spotColor = Color.LightGray
+                        )
                     //  .shadow(elevation = 2.dp, ambientColor = Color.Gray),
                     ,
                     label = { Text(text = "Email") },
@@ -111,14 +128,14 @@ fun LoginScreen(navController: NavController) {
                     },
                     isError = emailError,
                     singleLine = true,
-                    supportingText = {
-                        if (emailError) {
-                            Text(
-                                text = "Email cannot be empty",
-                                color = MaterialTheme.colorScheme.error
-                            )
-                        }
-                    },
+//                    supportingText = {
+//                        if (emailError) {
+//                            Text(
+//                                text = "Email cannot be empty",
+//                                color = MaterialTheme.colorScheme.error
+//                            )
+//                        }
+//                    },
                     colors = TextFieldDefaults.colors(
                         unfocusedContainerColor = textFieldColor,
                         focusedContainerColor = textFieldColor,
@@ -128,8 +145,14 @@ fun LoginScreen(navController: NavController) {
                 )
                 TextField(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp)
+                        .width(screenWidth * 90 / 100)
+                        .align(alignment = Alignment.CenterHorizontally)
+                        .shadow(
+                            elevation = 6.dp,
+                            shape = MaterialTheme.shapes.small,
+                            ambientColor = Color.Gray,
+                            spotColor = Color.LightGray
+                        )
                     // .shadow(elevation = 2.dp, ambientColor = Color.Gray)
                     //      .border(width = 1.dp, color = Color.Transparent),
                     ,
@@ -162,14 +185,14 @@ fun LoginScreen(navController: NavController) {
                     },
                     singleLine = true,
                     isError = passwordError,
-                    supportingText = {
-                        if (passwordError) {
-                            Text(
-                                text = "Password cannot be empty",
-                                color = MaterialTheme.colorScheme.error
-                            )
-                        }
-                    },
+//                    supportingText = {
+//                        if (passwordError) {
+//                            Text(
+//                                text = "Password cannot be empty",
+//                                color = MaterialTheme.colorScheme.error
+//                            )
+//                        }
+//                    },
                     colors = TextFieldDefaults.colors(
                         unfocusedContainerColor = textFieldColor,
                         focusedContainerColor = textFieldColor,
@@ -197,17 +220,33 @@ fun LoginScreen(navController: NavController) {
 
                             //will navigate to Home screen
                             navController.navigate(Screens.HomeScreen.route)
+                        } else {
+                            scope.launch {
+                                SnackbarHostState().showSnackbar(
+                                    message = "You invalid info",
+                                    duration = SnackbarDuration.Short
+                                )
+                            }
                         }
-                        //else show some error massage
 
-                    }, shape = RoundedCornerShape(100.dp), modifier = Modifier.fillMaxWidth(),
+
+                    },
+                    shape = RoundedCornerShape(100.dp),
+                    modifier = Modifier
+                        .height(screenHeight * 6 / 100)
+                        .width(screenWidth * 90 / 100)
+                        .align(alignment = Alignment.CenterHorizontally),
                     colors = ButtonDefaults.buttonColors(
                         containerColor = colorResource(
                             id = R.color.button_color
                         )
                     )
                 ) {
-                    Text(text = "Sign in", style = MaterialTheme.typography.headlineLarge, color = Color.White)
+                    Text(
+                        text = "Sign in",
+                        style = MaterialTheme.typography.headlineLarge,
+                        color = Color.White
+                    )
                 }
                 TextButton(
                     onClick = {
