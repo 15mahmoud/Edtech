1838fd805f9f1cd1d6571e81c48ae083ec5f4da1
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2025-02-12 05:08:51 +0200

    finishing review section

--- app/src/main/java/com/example/student_project/ui/screen/details/course/CourseLesson.kt
+++ app/src/main/java/com/example/student_project/ui/screen/details/course/CourseLesson.kt
@@ -4,8 +4,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 @Composable
-fun CourseLessonScreen(navController: NavController, videoUrl: String?){
-Text(text = videoUrl.toString(), fontSize = 22.sp)
+fun CourseLessonScreen(navController: NavController, encodedUrl: String?) {
+
+    val videoUrl = URLDecoder.decode(encodedUrl.toString(), StandardCharsets.UTF_8.toString())
+
+    Text(text = videoUrl.toString(), fontSize = 22.sp)
 }
