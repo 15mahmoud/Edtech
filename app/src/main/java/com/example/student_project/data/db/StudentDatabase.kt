2466b3c5d4c74e75e25fd59c21de5f0027c9251c
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2025-05-16 00:50:49 +0300

    changing app name + launcher icon

--- app/src/main/java/com/example/student_project/data/db/StudentDatabase.kt
+++ app/src/main/java/com/example/student_project/data/db/StudentDatabase.kt
@@ -3,7 +3,6 @@ package com.example.student_project.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.example.student_project.data.model.Instructor
 import com.example.student_project.data.model.User
 import com.example.student_project.util.ListConverter
 
